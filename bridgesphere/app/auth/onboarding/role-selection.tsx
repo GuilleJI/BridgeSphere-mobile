4df23fdd95007d8f24b7921964a01004b1f638c4
Guillermo Jimenez Iriarte <122047330+GuilleJI@users.noreply.github.com>
2025-04-24 22:52:14 -0300

    Fix navigation paths in Login and RoleSelection components for correct routing

--- bridgesphere/app/auth/onboarding/role-selection.tsx
+++ bridgesphere/app/auth/onboarding/role-selection.tsx
@@ -7,9 +7,9 @@ export default function RoleSelection(){
         <View>
             <h1>Role Selection Section</h1>
             {/* Role Selection button; on press, routes to one of the three onboarding pages: Client, Agent, and Brokerage */}
-            <Button title='Client' onPress={() => router.push('/onboarding/client-onboarding')} />
-            <Button title='Agent' onPress={()=> router.push('/onboarding/agent-onboarding')} />
-            <Button title='Brokerage' onPress={()=> router.push('/onboarding/brokerage-onboarding')}/>
+            <Button title='Client' onPress={() => router.push('../onboarding/client-onboarding')} />
+            <Button title='Agent' onPress={()=> router.push('../onboarding/agent-onboarding')} />
+            <Button title='Brokerage' onPress={()=> router.push('../onboarding/brokerage-onboarding')}/>
         </View>
     )
 }
